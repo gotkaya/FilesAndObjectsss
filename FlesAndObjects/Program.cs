faba795663a5192cd3024d6246724c1c2d9b7168
Opilane TTHK <opilane@tthk.ee>
2020-12-03 12:52:44 +0200

    first commit

--- FlesAndObjects/Program.cs
+++ FlesAndObjects/Program.cs
@@ -39,6 +39,19 @@ namespace FlesAndObjects
             {
                 Console.WriteLine($"title:{movie.title}; Rating:{movie.rating}; Year: {movie.year}");
             }
+
+            Console.WriteLine("what is your favorite movie? enter the title:");
+            string favMovieTitle = Console.ReadLine();
+            Console.WriteLine("Enter your favorite movie rating:");
+            string favMovieRating = Console.ReadLine();
+            Console.WriteLine("Enter the release year:");
+            string favMovieYear = Console.ReadLine();
+
+            Movie favMovie = new Movie(favMovieTitle, favMovieRating, favMovieYear);
+            string favMovieLine = $"{favMovie.title};{favMovie.rating};{favMovie.year}";
+
+            movieList.Add(favMovieLine);
+            File.WriteAllLines(Path.Combine(filePath, fileName), movieList);
         }
     }
 }
---
+++ ReadTotalFromShoppingList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadTotalFromShoppingList
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int price;
+
+            public Item(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+                Console.WriteLine($"Item:{name} created.");
+            }
+
+            public string Name { get { return name; } }
+            public int Price { get { return price; } }
+        }
+        static void Main(string[] args)
+        {
+            ReadFromShoppingList();
+        }
+
+        public static void ReadFromShoppingList()
+        {
+            string filePath = @"C:\Users\opilane\samples";
+            string fileName = @"shoppingList.txt";
+
+            List<Item> shoppingItems = new List<Item>();
+
+            List<string> LinesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+            foreach (string line in LinesFromFile)
+            {
+                    string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Item newItem = new Item(tempArray[0], Int32.Parse(tempArray[1]));
+                shoppingItems.Add(newItem);
+            }
+            Console.WriteLine("Your shopping cart:");
+            foreach(Item item in shoppingItems)
+            {
+                Console.WriteLine($"Item:{item.Name}; Price:{item.Price}");
+            }
+        }
+    }
+}
